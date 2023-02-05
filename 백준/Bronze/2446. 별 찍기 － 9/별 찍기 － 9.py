25adfcf1234558d269723627097a1f98b78b359f
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2023-02-05 13:10:06 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 160 ms, Memory: 116856 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+# n = 5
+nn = 2*n-1
+
+
+for i in range(n):
+    for j in range(nn+1):
+        if i > j:
+            print(' ',end="")
+        elif j > nn-i-1:
+            continue
+            # print(' ',end="")
+        else:
+            print('*',end="")
+    print()
+for i in range(1,n):
+    for j in range(nn+1):
+        if j < n-i-1:
+            print(' ',end="")
+        elif j > n+i-1:
+            continue
+            # print(' ',end="")
+        else:
+            print('*',end="")
+    print()
+
