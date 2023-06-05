8639370f03a8d000bb2106525849f9f0eac046bf
Minseok Jung <nminseok0917@naver.com>
2023-06-05 16:38:06 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.07 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,15 @@
+function solution(age) {
+    const ages = {
+        0:'a',
+        1:'b',
+        2:'c',
+        3:'d',
+        4:'e',
+        5:'f',
+        6:'g', 
+        7:'h',
+        8:'i',
+        9:'j',
+    };
+    return (""+age).split("").map( v => ages[v] ).join('')
+}
