1f11af39633b88909db8f32dcbecb35661a214ea
Minseok Jung <nminseok0917@naver.com>
2023-07-14 16:00:32 +0900

    [unrated] Title: a와 b 출력하기, Time: 68.18 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -0,0 +1,9 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}).on('line',line => {
+    const [a,b] = line.split(' ');
+    console.log(`a = ${a}`);
+    console.log(`b = ${b}`);
+})
