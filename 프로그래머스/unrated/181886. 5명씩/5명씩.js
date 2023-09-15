c8eee0e2acc0608c8006a0200bc533bdc49a5b2c
Minseok Jung <nminseok0917@naver.com>
2023-09-15 13:49:14 +0900

    [unrated] Title: 5명씩, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.js
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.js
@@ -1,8 +1,3 @@
 function solution(names) {
-    const answer = [];
-    while(names.length){
-        const [name] = names.splice(0,5)
-        answer.push(name);
-    }
-    return answer;
+    return names.filter( (_,i) => i%5 === 0 )
 }
