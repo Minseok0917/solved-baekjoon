049e0676d6251911398d7dd614491224c8667400
Minseok Jung <nminseok0917@naver.com>
2023-09-15 14:27:24 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,4 @@
+function solution(num_list) {
+    const {even, odd} = num_list.reduce( (acc,n,i) => ({ ...acc, [i%2 ? 'odd' : 'even']:acc[i%2 ? 'odd' : 'even'] + n }),{even:0, odd:0});
+    return even > odd ? even : odd;
+}
