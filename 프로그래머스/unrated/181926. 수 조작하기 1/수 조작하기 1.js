30be81f8aaf2028fac94b3ef6bc9275adf841443
Minseok Jung <nminseok0917@naver.com>
2023-09-15 10:20:59 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.58 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,4 +1,4 @@
 function solution(n, control) {
-    const {w,s,d,a} = [...control].reduce( (acc,c) => ({...acc, [c]:(acc[c]??0)+1 }),{});
-    return n + w*1 + s*-1 + d*10 + a*-10;
+    const o = { w:1, s:-1, d:10, a:-10};
+    return [...control].reduce( (acc,c) => acc+o[c] ,n);
 }
