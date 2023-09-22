77b5f3a7e632ad8f298ad71c83807d2f4c90e0e8
Minseok Jung <nminseok0917@naver.com>
2023-09-22 09:45:43 +0900

    [unrated] Title: 접미사 배열, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return Array.from( Array(my_string.length), (_,idx) => my_string.slice(my_string.length-idx-1,my_string.length) ).sort()
+    return Array.from( Array(my_string.length), (_,idx) => my_string.slice(idx) ).sort()
 }
